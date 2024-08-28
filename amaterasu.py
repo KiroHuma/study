a059c9f28a04fe2127a23c11b6e0df5aae85c998
TheLastVelo <jarellchinn44@gmail.com>
2024-08-28 15:38:17 -0700

    Caspians Solution Complete

--- amaterasu.py
+++ amaterasu.py
@@ -55,6 +55,45 @@
 # print(rect.area())
 
 
+# import threading
+# import time
+# import random
+
+# class Chopstick:
+#     def __init__(self):
+#         self.lock = threading.Lock()
+
+# class Philosopher(threading.Thread):
+#     def __init__(self, left_chopstick, right_chopstick):
+#         threading.Thread.__init__(self)
+#         self.left_chopstick = left_chopstick
+#         self.right_chopstick = right_chopstick
+
+#     def run(self):
+#         while True:
+#             self.think()
+#             self.eat()
+
+#     def think(self):
+#         print(f"{self.name} is thinking")
+#         time.sleep(random.uniform(1, 3))
+
+#     def eat(self):
+#         with self.left_chopstick.lock:
+#             with self.right_chopstick.lock:
+#                 print(f"{self.name} is eating")
+#                 time.sleep(random.uniform(1, 3))
+
+# if __name__ == "__main__":
+#     chopsticks = [Chopstick() for _ in range(5)]
+#     philosophers = [Philosopher(chopsticks[i], chopsticks[(i + 1) % 5]) for i in range(5)]
+
+#     for philosopher in philosophers:
+#         philosopher.start()
+
+#     for philosopher in philosophers:
+#         philosopher.join()
+
 import threading
 import time
 import random
@@ -63,30 +102,57 @@ class Chopstick:
     def __init__(self):
         self.lock = threading.Lock()
 
+class Arbitrator:
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.available = [True] * 5
+    
+    def request(self, left_index, right_index):
+        while True:
+            with self.lock:
+                print(f"Arbitrator is checking availability of chopsticks {left_index} and {right_index}.")
+                if self.available[left_index] and self.available[right_index]:
+                    self.available[left_index] = False
+                    self.available[right_index] = False
+                    print(f"Chopsticks {left_index} and {right_index} are granted to philosopher.")
+                    return
+                else:
+                    print(f"Chopsticks {left_index} and {right_index} are not available. Philosopher must wait.")
+            time.sleep(random.uniform(0.1, 0.5))
+
+    def release(self, left_index, right_index):
+        with self.lock:
+            self.available[left_index] = True
+            self.available[right_index] = True
+            print(f"Chopsticks {left_index} and {right_index} have been released back to the table.")
+
 class Philosopher(threading.Thread):
-    def __init__(self, left_chopstick, right_chopstick):
+    def __init__(self, philosopher_number, left_chopstick, right_chopstick, arbitrator):
         threading.Thread.__init__(self)
+        self.philosopher_number = philosopher_number
         self.left_chopstick = left_chopstick
         self.right_chopstick = right_chopstick
+        self.arbitrator = arbitrator
 
     def run(self):
         while True:
             self.think()
+            self.arbitrator.request(self.philosopher_number, (self.philosopher_number + 1) % 5)
             self.eat()
+            self.arbitrator.release(self.philosopher_number, (self.philosopher_number + 1) % 5)
 
     def think(self):
-        print(f"{self.name} is thinking")
+        print(f"Philosopher {self.philosopher_number} is thinking.")
         time.sleep(random.uniform(1, 3))
-
+    
     def eat(self):
-        with self.left_chopstick.lock:
-            with self.right_chopstick.lock:
-                print(f"{self.name} is eating")
-                time.sleep(random.uniform(1, 3))
+        print(f"Philosopher {self.philosopher_number} is eating.")
+        time.sleep(random.uniform(1, 3))
 
 if __name__ == "__main__":
     chopsticks = [Chopstick() for _ in range(5)]
-    philosophers = [Philosopher(chopsticks[i], chopsticks[(i + 1) % 5]) for i in range(5)]
+    arbitrator = Arbitrator()
+    philosophers = [Philosopher(i, chopsticks[i], chopsticks[(i + 1) % 5], arbitrator) for i in range(5)]
 
     for philosopher in philosophers:
         philosopher.start()
