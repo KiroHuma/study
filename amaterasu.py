0017d04499919b6ebe7cdd38d9797f12652809ed
TheLastVelo <jarellchinn44@gmail.com>
2024-08-28 15:46:05 -0700

    Caspians Solution more readable in terminal

--- amaterasu.py
+++ amaterasu.py
@@ -63,7 +63,7 @@
 #     def __init__(self):
 #         self.lock = threading.Lock()
 
-# class Philosopher(threading.Thread):
+# class Genius(threading.Thread):
 #     def __init__(self, left_chopstick, right_chopstick):
 #         threading.Thread.__init__(self)
 #         self.left_chopstick = left_chopstick
@@ -86,13 +86,79 @@
 
 # if __name__ == "__main__":
 #     chopsticks = [Chopstick() for _ in range(5)]
-#     philosophers = [Philosopher(chopsticks[i], chopsticks[(i + 1) % 5]) for i in range(5)]
+#     geniuss = [Genius(chopsticks[i], chopsticks[(i + 1) % 5]) for i in range(5)]
 
-#     for philosopher in philosophers:
-#         philosopher.start()
+#     for genius in geniuss:
+#         genius.start()
 
-#     for philosopher in philosophers:
-#         philosopher.join()
+#     for genius in geniuss:
+#         genius.join()
+
+# import threading
+# import time
+# import random
+
+# class Chopstick:
+#     def __init__(self):
+#         self.lock = threading.Lock()
+
+# class Arbitrator:
+#     def __init__(self):
+#         self.lock = threading.Lock()
+#         self.available = [True] * 5
+    
+#     def request(self, left_index, right_index):
+#         while True:
+#             with self.lock:
+#                 print(f"Arbitrator is checking availability of chopsticks {left_index} and {right_index}.")
+#                 if self.available[left_index] and self.available[right_index]:
+#                     self.available[left_index] = False
+#                     self.available[right_index] = False
+#                     print(f"Chopsticks {left_index} and {right_index} are granted to genius.")
+#                     return
+#                 else:
+#                     print(f"Chopsticks {left_index} and {right_index} are not available. Genius must wait.")
+#             time.sleep(random.uniform(0.1, 0.5))
+
+#     def release(self, left_index, right_index):
+#         with self.lock:
+#             self.available[left_index] = True
+#             self.available[right_index] = True
+#             print(f"Chopsticks {left_index} and {right_index} have been released back to the table.")
+
+# class Genius(threading.Thread):
+#     def __init__(self, genius_number, left_chopstick, right_chopstick, arbitrator):
+#         threading.Thread.__init__(self)
+#         self.genius_number = genius_number
+#         self.left_chopstick = left_chopstick
+#         self.right_chopstick = right_chopstick
+#         self.arbitrator = arbitrator
+
+#     def run(self):
+#         while True:
+#             self.think()
+#             self.arbitrator.request(self.genius_number, (self.genius_number + 1) % 5)
+#             self.eat()
+#             self.arbitrator.release(self.genius_number, (self.genius_number + 1) % 5)
+
+#     def think(self):
+#         print(f"Genius {self.genius_number} is thinking.")
+#         time.sleep(random.uniform(1, 3))
+    
+#     def eat(self):
+#         print(f"Genius {self.genius_number} is eating.")
+#         time.sleep(random.uniform(1, 3))
+
+# if __name__ == "__main__":
+#     chopsticks = [Chopstick() for _ in range(5)]
+#     arbitrator = Arbitrator()
+#     geniuss = [Genius(i, chopsticks[i], chopsticks[(i + 1) % 5], arbitrator) for i in range(5)]
+
+#     for genius in geniuss:
+#         genius.start()
+
+#     for genius in geniuss:
+#         genius.join()
 
 import threading
 import time
@@ -107,29 +173,24 @@ class Arbitrator:
         self.lock = threading.Lock()
         self.available = [True] * 5
     
-    def request(self, left_index, right_index):
+    def request(self, left_index, right):
         while True:
             with self.lock:
-                print(f"Arbitrator is checking availability of chopsticks {left_index} and {right_index}.")
-                if self.available[left_index] and self.available[right_index]:
+                if self.available[left_index] and self.available[right]:
                     self.available[left_index] = False
-                    self.available[right_index] = False
-                    print(f"Chopsticks {left_index} and {right_index} are granted to philosopher.")
+                    self.available[right] = False
                     return
-                else:
-                    print(f"Chopsticks {left_index} and {right_index} are not available. Philosopher must wait.")
             time.sleep(random.uniform(0.1, 0.5))
 
-    def release(self, left_index, right_index):
+    def release(self, left_index, right):
         with self.lock:
             self.available[left_index] = True
-            self.available[right_index] = True
-            print(f"Chopsticks {left_index} and {right_index} have been released back to the table.")
+            self.available[right] = True
 
-class Philosopher(threading.Thread):
-    def __init__(self, philosopher_number, left_chopstick, right_chopstick, arbitrator):
+class Genius(threading.Thread):
+    def __init__(self, genius_number, left_chopstick, right_chopstick, arbitrator):
         threading.Thread.__init__(self)
-        self.philosopher_number = philosopher_number
+        self.genius_number = genius_number
         self.left_chopstick = left_chopstick
         self.right_chopstick = right_chopstick
         self.arbitrator = arbitrator
@@ -137,25 +198,25 @@ class Philosopher(threading.Thread):
     def run(self):
         while True:
             self.think()
-            self.arbitrator.request(self.philosopher_number, (self.philosopher_number + 1) % 5)
+            self.arbitrator.request(self.genius_number, (self.genius_number + 1) % 5)
             self.eat()
-            self.arbitrator.release(self.philosopher_number, (self.philosopher_number + 1) % 5)
+            self.arbitrator.release(self.genius_number, (self.genius_number + 1) % 5)
 
     def think(self):
-        print(f"Philosopher {self.philosopher_number} is thinking.")
+        print(f"Genius {self.genius_number} is thinking")
         time.sleep(random.uniform(1, 3))
     
     def eat(self):
-        print(f"Philosopher {self.philosopher_number} is eating.")
+        print(f"Genius {self.genius_number} is eating")
         time.sleep(random.uniform(1, 3))
 
 if __name__ == "__main__":
     chopsticks = [Chopstick() for _ in range(5)]
     arbitrator = Arbitrator()
-    philosophers = [Philosopher(i, chopsticks[i], chopsticks[(i + 1) % 5], arbitrator) for i in range(5)]
+    geniuss = [Genius(i, chopsticks[i], chopsticks[(i + 1) % 5], arbitrator) for i in range(5)]
 
-    for philosopher in philosophers:
-        philosopher.start()
+    for genius in geniuss:
+        genius.start()
 
-    for philosopher in philosophers:
-        philosopher.join()
+    for genius in geniuss:
+        genius.join()
